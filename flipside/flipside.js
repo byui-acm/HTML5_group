59382fe19726bd9967231c099dee648ae0a53ca0
Steven Lambert <straker@Stevens-MacBook-Pro-2.local>
2013-10-18 11:02:49 -0600

    Added Unit base class and Warrior, Archer, Wizard functions

--- flipside/flipside.js
+++ flipside/flipside.js
@@ -1,19 +1,19 @@
-
 // Constants
-var LENGTH        = 8;
-var TILE_SIZE     = 50;
+var LENGTH        = 8;    // size of the board
+var TILE_SIZE     = 50;   // size of a tile
 
 // Variables
-var lightBoard    = {};
-var darkBoard     = {};
-var canvasL       = document.getElementById('light');
-var canvasD       = document.getElementById('dark');
+var whiteBoard    = {};
+var blackBoard    = {};
+var canvasL       = document.getElementById('white');
+var canvasD       = document.getElementById('black');
 var ctxL          = canvasL.getContext('2d');
-var ctxD          = canvasD.getContext('2d');
-var clickFunction = {};
+var ctxB          = canvasD.getContext('2d');
+var clickFunction = {};   // the function to perform on a click
 
-/*
+/**
  * Onclick event
+ * @param event The mouse event object
  */
 clickHandler = function(event) {
   var mouseX = event.offsetX || event.layerX;
@@ -28,8 +28,10 @@ canvasL.onclick = function(event) {clickHandler(event)};
 canvasD.onclick = function(event) {clickHandler(event)};
 clickFunction = flipRookTiles;
 
-/*
+/**
  * Change the click handler for flipping
+ * @param name The id attribute of the button to activate
+ * @param func The function object to assign as the new clickFunction
  */
 function changeClickHandler(name, func) {
   clickFunction = func;
@@ -40,8 +42,11 @@ function changeClickHandler(name, func) {
   $('#'+name).addClass('active');
 }
 
-/*
- * Create a new board with the color provided
+/**
+ * Create a new board
+ * @param color The color to assign all new tiles of the board
+ *
+ * @return The new board array
  */
 function makeBoard(color) {
   var board = [];
@@ -56,72 +61,76 @@ function makeBoard(color) {
   return board;
 }
 
-/*
+/**
  * Reset the tiles on both boards
  */
 function reset() {
-  lightBoard = makeBoard("green");
-  darkBoard  = makeBoard("gray");
+  whiteBoard = makeBoard("green");
+  blackBoard = makeBoard("gray");
 
   drawBoards();
 }
 
-/*
+/**
  * Draw the boards and their tiles
  */
 function drawBoards() {
-  // Draw each tile
+  // draw each tile
   for (var row = 0; row < LENGTH; row++) {
     for (var col = 0; col < LENGTH; col++) {
-      ctxL.fillStyle = lightBoard[row][col].type;
+      ctxL.fillStyle = whiteBoard[row][col].type;
       ctxL.fillRect(row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 
-      ctxD.fillStyle = darkBoard[row][col].type;
-      ctxD.fillRect(row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+      ctxB.fillStyle = blackBoard[row][col].type;
+      ctxB.fillRect(row * TILE_SIZE, col * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
 
-  // Draw the tile lines
+  // draw the tile lines
   ctxL.beginPath();
-  ctxD.beginPath();
+  ctxB.beginPath();
   for (var i = 0; i < LENGTH; i++) {
     ctxL.moveTo(0, TILE_SIZE * i);
     ctxL.lineTo(canvasL.width, TILE_SIZE * i);
     ctxL.moveTo(TILE_SIZE * i, 0);
     ctxL.lineTo(TILE_SIZE * i, canvasL.height);
 
-    ctxD.moveTo(0, TILE_SIZE * i);
-    ctxD.lineTo(canvasD.width, TILE_SIZE * i);
-    ctxD.moveTo(TILE_SIZE * i, 0);
-    ctxD.lineTo(TILE_SIZE * i, canvasD.height);
+    ctxB.moveTo(0, TILE_SIZE * i);
+    ctxB.lineTo(canvasD.width, TILE_SIZE * i);
+    ctxB.moveTo(TILE_SIZE * i, 0);
+    ctxB.lineTo(TILE_SIZE * i, canvasD.height);
   }
   ctxL.stroke();
-  ctxD.stroke();
+  ctxB.stroke();
 }
 
-/*
+/**
  * Flip all horizontal and vertical tiles centered on row, col
+ * @param row The row to flip
+ * @param col The column to flip
  */
 function flipRookTiles(row, col) {
   var rowTemp, colTemp;
   for (var i = 0; i < LENGTH; i++) {
-    rowTemp = lightBoard[row][i];
-    colTemp = lightBoard[i][col];
+    rowTemp = whiteBoard[row][i];
+    colTemp = whiteBoard[i][col];
 
-    lightBoard[row][i] = darkBoard[row][i];
-    darkBoard[row][i] = rowTemp;
+    whiteBoard[row][i] = blackBoard[row][i];
+    blackBoard[row][i] = rowTemp;
 
     if (i != row) {
-      lightBoard[i][col] = darkBoard[i][col];
-      darkBoard[i][col] = colTemp;
+      whiteBoard[i][col] = blackBoard[i][col];
+      blackBoard[i][col] = colTemp;
     }
   }
 
   drawBoards();
 }
 
-/*
+/**
  * Flip all diagonal tiles centered on row, col
+ * @param row The center row of the flip
+ * @param col The center column of the flip
  */
 function flipBishTiles(row, col) {
   var row1, row2, temp;
@@ -130,19 +139,243 @@ function flipBishTiles(row, col) {
     row2 = row + (col - i);
 
     if (row1 < LENGTH && row1 >= 0) {
-      temp = lightBoard[row1][i];
-      lightBoard[row1][i] = darkBoard[row1][i];
-      darkBoard[row1][i] = temp;
+      temp = whiteBoard[row1][i];
+      whiteBoard[row1][i] = blackBoard[row1][i];
+      blackBoard[row1][i] = temp;
     }
 
     if (row2 < LENGTH && row2 >= 0 && row1 != row2) {
-      temp = lightBoard[row2][i];
-      lightBoard[row2][i] = darkBoard[row2][i];
-      darkBoard[row2][i] = temp;
+      temp = whiteBoard[row2][i];
+      whiteBoard[row2][i] = blackBoard[row2][i];
+      blackBoard[row2][i] = temp;
     }
   }
 
   drawBoards();
 }
+
+/**
+ * Asset pre-loader object. Loads all images and sounds
+ */
+assetLoader = new function() {
+  // images
+  this.imgs = {
+    'warrior' : new Image(),
+    'archer'  : new Image(),
+    'wizard'  : new Image()
+  };
+
+  // sounds
+  this.sounds = {};
+}
+
+/**
+ * Unit base class
+ */
+function Unit() {
+  // unit info
+  this.loc         = {     // current row and column on the board
+    'row'  : 0,
+    'col'  : 0
+  };
+  this.color       = '';   // white, black
+  this.type        = '';   // warrior, archer, wizard
+  img              = {};   // reference to the assetLoader img
+
+  // action stats
+  var actionLimit  = 2;    // how many actions the unit can perform per turn
+  var moveLimit    = 2;    // how many move actions the unit can perform per turn
+  var atkLimit     = 1;    // how many attack actions the unit can perform per turn
+  var moveActions  = 0;    // the current move actions the unit has performed this turn
+  var atkActions   = 0;    // the current attack actions the unit has performed this turn
+
+  // unit stats
+  this.hp           = 0;    // health
+  this.spd          = 0;    // movement speed (how far unit can move)
+  this.rng          = 0;    // attack range (how far unit can attack)
+  this.dmg          = {     // damage range, low-high
+    'low'  : 0,
+    'high' : 0
+  };
+
+  /**
+   * Draw the unit to the board
+   * @param ctx The canvas context to draw on
+   */
+  this.draw = function(ctx) {
+    ctx.drawImage(this.img, this.loc.row * TILE_SIZE, this.loc.col * TILE_SIZE);
+  };
+
+  /**
+   * Move to an empty square
+   * @param row The row to move to
+   * @param col The column to move to
+   *
+   * @return True if the unit can move, false otherwise
+   */
+  this.move = function(row, col) {
+    if (this.canMove() && this.inMoveRange(row, col)) {
+      this.loc.row = row;
+      this.loc.col = col;
+      moveActions++;
+      return true;
+    }
+
+    return false;
+  };
+
+  /**
+   * Performs the unit's attack action
+   * Function to be overwritten for each unit type
+   *
+   * @return True if the unit can use it's power, false otherwise
+   */
+  this.attack = function() {
+    if (this.canAtack()) {
+      atkActions  ++;
+      return true;
+    }
+
+    return false;
+  };
+
+  /**
+   * If the unit can move this turn
+   *
+   * @return True if the unit can move, false otherwise.
+   */
+  this.canMove = function() {
+    return moveActions < moveLimit;
+  };
+
+  /**
+   * If the unit can attack this turn
+   *
+   * @return True if the unit can use it's power, false otherwise
+   */
+  this.canAttack = function() {
+    return atkActions < atkLimit;
+  };
+
+  /**
+   * Check if the row and column are within the unit's move range
+   * @param row The row to check
+   * @param col The column to check
+   *
+   * @return True if row, col is within range, false otherwise
+   */
+  this.inMoveRange = function(row, col) {
+    var rangeRow = Math.abs(this.loc.row - row);
+    var rangeCol = Math.abs(this.loc.col - col);
+
+    return rangeCol + rangeRow <= this.spd;
+  };
+
+  /**
+   * Check if the row and column are within the unit's attack range
+   * @param row The row to check
+   * @param col The column to check
+   *
+   * @return True if row, col is within range, false otherwise
+   */
+  this.inAtkRange = function(row, col) {
+    var rangeRow = Math.abs(this.loc.row - row);
+    var rangeCol = Math.abs(this.loc.col - col);
+
+    return rangeCol + rangeRow <= this.rng;
+  };
+
+  /**
+   * Reset the unit's move and power actions
+   */
+  this.resetActions = function() {
+    moveActions  = 0;
+    atkActions   = 0;
+  };
+}
+
+/**
+ * Warrior class
+ * @param color The color to assign the unit
+ */
+function Warrior(color) {
+  this.color = color;
+  this.type  = 'Warrior';
+  this.img   = assetLoader.imgs.warrior;
+
+  this.hp    = 0;
+  this.spd   = 0;
+  this.rng   = 1;
+  this.dmg   = {
+    'low'  : 0,
+    'high' : 0
+  };
+
+  /**
+   * Performs the unit's attack action
+   *
+   * @return True if the unit can use it's power, false otherwise
+   */
+  this.attack = function() {
+    return;
+  };
+}
+Warrior.prototype = new Unit(); // set up inheritance
+
+/**
+ * Archer class
+ * @param color The color to assign the unit
+ */
+function Archer(color) {
+  this.color = color;
+  this.type  = 'Archer';
+  this.img   = assetLoader.imgs.archer;
+
+  this.hp    = 0;
+  this.spd   = 0;
+  this.rng   = 0;
+  this.dmg   = {
+    'low'  : 0,
+    'high' : 0
+  };
+
+  /**
+   * Performs the unit's attack action
+   *
+   * @return True if the unit can use it's attack, false otherwise
+   */
+  this.attack = function() {
+    return;
+  };
+}
+Archer.prototype = new Unit(); // set up inheritance
+
+/**
+ * Wizard class
+ * @param color The color to assign the unit
+ */
+function Wizard(color) {
+  this.color = color;
+  this.type  = 'Wizard';
+  this.img   = assetLoader.imgs.wizard;
+
+  this.hp    = 0;
+  this.spd   = 0;
+  this.rng   = 0;
+  this.dmg   = {
+    'low'  : 0,
+    'high' : 0
+  };
+
+  /**
+   * Performs the unit's attack action
+   *
+   * @return True if the unit can use it's power, false otherwise
+   */
+  this.attack = function() {
+    return;
+  };
+}
+Wizard.prototype = new Unit(); // set up inheritance
 
 reset();
